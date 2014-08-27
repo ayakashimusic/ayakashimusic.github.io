12beb5a57e2c4075d3d0e0587ef90598e7bce6d0
gratoo <gratoo@gmail.com>
2014-08-26 22:09:24 -0700

    menu - social

---
+++ scripts/ayakashi_cube.js
@@ -0,0 +1,61 @@
+var camera, scene, renderer, canvas;
+var loader, mesh, light;
+
+var canvasHolder = $("#CanvasHolder");
+var isAyaCubeLoaded = false;
+
+window.addEventListener( 'resize', onWindowResize, false );
+
+init();
+animate();
+
+function init() {
+    scene = new THREE.Scene();
+    
+    camera = new THREE.PerspectiveCamera( 75, canvasHolder.width() / canvasHolder.height(), 1, 10000 );
+    camera.position.z = 350;
+    camera.position.y = -14;
+
+    loader = new THREE.JSONLoader();
+
+    loader.load( "3D/Aya_01.js", function( geometry, materials ) {
+        //mesh = new THREE.Mesh( geometry , new THREE.MeshFaceMaterial( materials ) );
+
+        var newMat = new THREE.MeshFaceMaterial( materials );
+        
+        //DEBUG
+        //console.log(newMat);
+        //newMat.materials[0] = new THREE.MeshLambertMaterial( { color: 0x99FF99 } );
+        //newMat.materials[1] = new THREE.MeshLambertMaterial( { color: 0x000088 } );
+
+        mesh = new THREE.Mesh( geometry , newMat );
+
+        scene.add( mesh );
+        mesh.rotation.x += 0.6;
+        isAyaCubeLoaded = true;
+    } );
+
+    // A light shining from the direction of the camera.
+    light = new THREE.DirectionalLight("#fff", 2);
+    light.position.set(0,-0.2,1);
+    scene.add(light);
+
+    canvas = document.getElementById("CubeCanvas") ;
+    renderer = new THREE.WebGLRenderer( {canvas: canvas, alpha: true} );
+    renderer.setSize( canvasHolder.width(), canvasHolder.height() );
+}
+
+function animate() {
+    requestAnimationFrame( animate );
+
+    if(isAyaCubeLoaded){
+        mesh.rotation.y += 0.01;
+        renderer.render( scene, camera );
+    }
+}
+
+function onWindowResize(){
+    camera.aspect = canvasHolder.width() / canvasHolder.height();
+    camera.updateProjectionMatrix();
+    renderer.setSize( canvasHolder.width(), canvasHolder.height() );
+}
---
+++ scripts/menu.js
@@ -0,0 +1,133 @@
+function getPointsOnCircle(center, radius, count) {
+    var angle = 360/count;
+    var startPoints = [];
+    
+    for (var i = angle/2; i < 360; i += angle) {
+        var j = i * Math.PI / 180;
+        var x = radius * Math.sin(j);
+        var y = radius * Math.cos(j);
+        
+        x += center.x;
+        y += center.y;
+        
+        startPoints[startPoints.length] = new Point(x, y);
+        //console.log(x, y);
+    }
+    
+    //console.log(startPoints);
+    return startPoints;
+}
+
+function drawCircle(center, radius, color, width){
+    var path = new Path.Circle(center, radius);
+    path.strokeColor = color;
+    path.strokeWidth = width;
+}
+
+function drawPoints(pointArray){
+    var path = new Path.Circle(center, 2);
+    path.fillColor = 'black';
+    
+    var symbol = new Symbol(path);
+    
+    for(var i=0; i<pointArray.length; i++){
+        symbol.place(pointArray[i]);
+    }
+}
+
+function drawLines(center, pointArray, length, color, width){
+    var justifiedPoints = [];
+    
+    for(var i=0; i<pointArray.length; i++){
+        var start = pointArray[i];
+        var vector = start - center;
+        vector = vector.normalize();
+        var end = start + vector * length;
+        
+        var line = new Path();
+        line.strokeColor = color;
+        line.add(start);
+        line.add(end);
+        
+        //console.log(vector);
+        
+        var justification = 'left';
+        
+        if(vector.x > 0){
+            end = end + new Point(length,0);
+            line.add(end);
+            justification = 'left';
+        }
+        else{
+            end = end + new Point(-length,0);
+            line.add(end);
+            justification = 'right';
+        }
+        
+        line.strokeWidth = width;
+        //line.strokeCap = 'round';
+
+        justifiedPoints[justifiedPoints.length] = {position: end, justification: justification};
+    }
+    
+    return justifiedPoints;
+}
+
+function drawText(justifiedPoints, menuItems, spacing, color){
+    for(var i=0; i<justifiedPoints.length; i++){
+        var text = new PointText({
+            fontSize: 30,
+            fillColor: color,
+            justification: justifiedPoints[i].justification,
+            point: justifiedPoints[i].position,
+            content: menuItems[i]
+        });
+        
+        if(text.justification == 'left'){
+            text.point += spacing;
+        }
+        else{
+            text.point -= spacing;
+        }
+        
+        text.onMouseEnter = function(event) {
+            this.fillColor = 'white';
+        }
+
+        text.onMouseLeave = function(event) {
+            this.fillColor = color;
+        }
+    }
+}
+
+function onResize(event) {
+    setSize();
+}
+
+function setSize(){
+    center = new Point(canvas.width()/2, canvas.height()/2);
+    radius = canvas.height()/3;
+}
+
+function start(){
+    var menuItems = ["Info", "Music", "Bio", "Shows", "Misc", "Misc"]
+    var count = menuItems.length;
+    var length = 40;
+    var color = "#ECF0FF";
+    var textSpacing = new Point(10, 0);
+    var lineWidth = 6;
+
+    var circlePoints = getPointsOnCircle(center, radius, count);
+    //drawPoints([center]);
+    //drawPoints(circlePoints);
+    drawCircle(center, radius, color, lineWidth);
+    var justifiedPoints = drawLines(center, circlePoints, length, color, lineWidth);
+    drawText(justifiedPoints, menuItems, textSpacing, "blue");
+}
+
+
+// START //
+var canvas = $("#CanvasHolder");
+var center, radius;
+setSize();
+start();
